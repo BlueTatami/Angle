3df3f21fcf20b9c4c7f35f2c734b4f012427c55a
Alexandre Lopoukhine <superlopuh@gmail.com>
2015-12-09 16:21:18 +0000

    Created Angle framework project

---
+++ Angle/Angle.h
@@ -0,0 +1,19 @@
+//
+//  Angle.h
+//  Angle
+//
+//  Created by Alexandre Lopoukhine on 09/12/2015.
+//  Copyright © 2015 bluetatami. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Angle.
+FOUNDATION_EXPORT double AngleVersionNumber;
+
+//! Project version string for Angle.
+FOUNDATION_EXPORT const unsigned char AngleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Angle/PublicHeader.h>
+
+
